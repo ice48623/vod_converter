88fccec8c2726efdca1ae5985550b6fc902428bc
Khemmatat Theanvanichpant <khemmatat.thean@gmail.com>
2018-11-29 20:33:12 +0700

    Edit convert script to handle path with whitespace

--- convert.sh
+++ convert.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 
-INPUT_FILE=$1
-PIXEL_DIMENSION=$2
-OUTPUT_FILE=$3
+INPUT_FILE="$1"
+PIXEL_DIMENSION="$2"
+OUTPUT_FILE="$3"
 
-ffmpeg -i $INPUT_FILE -vf scale=$PIXEL_DIMENSION -c:v libx264 -preset ultrafast $OUTPUT_FILE
+ffmpeg -i "$INPUT_FILE" -vf scale="$PIXEL_DIMENSION" -c:v libx264 -preset ultrafast "$OUTPUT_FILE"
